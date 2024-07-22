5e24b25f8e452699ad31d47097f2254edde4407a
MinhHanhTran122 <hanhtran1222003@gmail.com>
2024-07-22 23:35:07 +0700

    Nhung

---
+++ src/main/java/com/poly/du_an_tot_nghiep_f6/controller/SizeController.java
@@ -0,0 +1,10 @@
+package com.poly.du_an_tot_nghiep_f6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/size")
+public class SizeController {
+
+}
