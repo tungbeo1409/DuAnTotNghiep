f3b5e9169521f2c3ac132824e8aeed8a7516d852
tungbeo1409 <149397665+tungbeo1409@users.noreply.github.com>
2024-07-24 08:32:49 +0700

    Add WebConfig để public API
    Cho phép call api từ bên ngoài

--- src/main/java/com/poly/du_an_tot_nghiep_f6/controller/ProductDetailController.java
+++ src/main/java/com/poly/du_an_tot_nghiep_f6/controller/ProductDetailController.java
@@ -1,8 +1,11 @@
 package com.poly.du_an_tot_nghiep_f6.controller;
 
+import com.poly.du_an_tot_nghiep_f6.entity.Color;
 import com.poly.du_an_tot_nghiep_f6.entity.ProductDetail;
+import com.poly.du_an_tot_nghiep_f6.repository.ColorRepo;
 import com.poly.du_an_tot_nghiep_f6.repository.ProductDetailRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,16 @@ public class ProductDetailController {
     @Autowired
     ProductDetailRepo productDetailRepo;
 
+    @Autowired
+    ColorRepo colorRepo;
+
     @GetMapping("/product")
     public List<ProductDetail> getAllProductDetails() {
         return productDetailRepo.findAll();
+    }
+    @GetMapping("/color")
+    public List<Color> getAllColor() {
+        return colorRepo.findAll();
     }
 
 
